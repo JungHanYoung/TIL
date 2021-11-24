0097ef850831557eeaf305d3f8c5b27023657094
JungHanYoung <8735132@naver.com>
2021-11-24 14:37:43 +0900

    2021_11_24 --all.sh master

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+date=$(date +'%Y_%m_%d')
+
+git pull
+git add -A
+git commit -m "$date --all.sh master"
+git push origin master
+
+# build
+npm run docs:build
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy with vuepress'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:JungHanYoung/TIL.git master:gh-pages
+
+cd -
